f0108a405c6b22de8ca65f82a029409a73aa2f3c
austin-watkins <watkins.austin@gmail.com>
2017-11-10 16:43:57 -0700

    Added basic implementation to MemoryPalace class

--- src/MemoryPalace.java
+++ src/MemoryPalace.java
@@ -1,6 +1,24 @@
-/**
- * Created by awatki12 on 11/10/2017.
- */
-public class MemoryPalace {
 
+public class MemoryPalace {
+  private int size;
+  private Loci[] locis;
+  private String name;
+
+  public MemoryPalace(int size, String name) {
+    this.size = size;
+    locis = new Loci[size];
+    this.name = name;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public Loci[] getLocis() {
+    return locis;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
